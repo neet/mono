1fd0972f46b6fabed019f886fea9c60a8c8cf0ef
Ryo Igarashi <n33t5hin@gmail.com>
2025-10-10 03:29:55 +0900

    fix: Remove duplicated files

--- web/src/models/habit.ts
+++
@@ -1,9 +0,0 @@
-export interface Habit {
-  id: string;
-  rrule: string;
-  tzid: string;
-  title: string;
-  description: string;
-  created_at: string;
-  updated_at: string;
-}
--- web/src/models/task.ts
+++
@@ -1,8 +0,0 @@
-export interface Task {
-  id: string;
-  title: string;
-  description: string;
-  completed: boolean;
-  created_at: string;
-  updated_at: string;
-}
