0eec70e26f84181ac7f952e2cdc4ce6baad706f4
Ryo Igarashi <n33t5hin@gmail.com>
2025-11-24 17:09:26 +0900

    feat: Add links to other languages

--- web/components/Complementary.tsx
+++ web/components/Complementary.tsx
@@ -2,8 +2,9 @@ import { ArrowPathIcon, CheckIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
 import { useTranslations } from "next-intl";
 import { FC } from "react";
+import NextLink from "next/link";
 
-import { Link } from "@/i18n/navigation";
+import { Link, getPathname } from "@/i18n/navigation";
 
 export type ComplementaryProps = {
   className?: string;
@@ -14,21 +15,38 @@ export const Complementary: FC<ComplementaryProps> = (props) => {
   const { className } = props;
 
   return (
-    <nav className={clsx("p-4", className)}>
-      <ul className="flex flex-col gap-2">
-        <li>
-          <Link href="/" className="flex gap-1 items-center hover:underline">
-            <CheckIcon className="size-5" />
-            {t("tasks")}
-          </Link>
-        </li>
-        <li>
-          <Link href="/habits" className="flex gap-1 items-center hover:underline">
-            <ArrowPathIcon className="size-5" />
-            {t("habits")}
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className={clsx("flex flex-col h-full", className)}>
+      <nav className="grow p-4">
+        <ul className="flex flex-col gap-2">
+          <li>
+            <Link href="/" className="flex gap-1 items-center hover:underline">
+              <CheckIcon className="size-5" />
+              {t("tasks")}
+            </Link>
+          </li>
+          <li>
+            <Link href="/habits" className="flex gap-1 items-center hover:underline">
+              <ArrowPathIcon className="size-5" />
+              {t("habits")}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className="p-4">
+        <ul className="flex flex-wrap gap-2 text-sm">
+          <li>
+            <NextLink href={getPathname({ href: "/", locale: "en" })} className="underline">
+              English
+            </NextLink>
+          </li>
+          <li>
+            <NextLink href={getPathname({ href: "/", locale: "ja" })} className="underline">
+              日本語
+            </NextLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
