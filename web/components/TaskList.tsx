76df019f4e619dbb58c0c7601be2dd8059e34f77
Ryō Igarashi (五十嵐 涼) <n33t5hin@gmail.com>
2025-11-30 00:04:31 +0900

    feat: Add empty TaskList design (#25)

--- web/components/TaskList.tsx
+++ web/components/TaskList.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { clsx } from "clsx";
+import { getTranslations } from "next-intl/server";
 
 import { Task } from "../models/task";
 import { TaskListItem } from "./TaskListItem";
@@ -9,16 +10,28 @@ export type TaskProps = {
   readonly className?: string;
 }
 
-export const TaskList: FC<TaskProps > = (props) => {
+export const TaskList: FC<TaskProps > = async (props) => {
   const { tasks, className } = props;
+  const t = await getTranslations("components.TaskList");
 
   return (
     <div className={clsx("border-2 rounded", className)}>
-      <div className="divide-dashed divide-y-2">
-        {tasks.map((task) => (
-          <TaskListItem className="p-2" key={task.id} task={task} />
-        ))}
-      </div>
+      {tasks.length === 0 && (
+        <div className="p-4">
+          <div className="text-center">
+            <div className="text-4xl mb-2">🎉</div>
+            {t("empty")}
+          </div>
+        </div>
+      )}
+
+      {tasks.length > 0 && (
+        <div className="divide-dashed divide-y-2">
+          {tasks.map((task) => (
+            <TaskListItem className="p-2" key={task.id} task={task} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
