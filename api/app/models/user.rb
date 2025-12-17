e192bccba9a55158d8cd9bc2c0d72f52f3b8c8c6
Ryo Igarashi <n33t5hin@gmail.com>
2025-12-17 20:23:02 +0900

    feat: Introduce Doorkeeper

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
 
   has_many :tasks, dependent: :destroy
   has_many :habits, dependent: :destroy
+
+  # https://doorkeeper.gitbook.io/guides/ruby-on-rails/getting-started#integrating-with-existing-user-model
+  has_many :access_grants,
+           class_name: "Doorkeeper::AccessGrant",
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+  has_many :access_tokens,
+           class_name: "Doorkeeper::AccessToken",
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
 end
